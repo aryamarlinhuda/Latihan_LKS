8587ba457d0a1b97e0ebd4551e626a4b58e2ca52
aryamarlinhuda <aryamarlin12@gmail.com>
2023-02-04 21:07:08 +0700

    h-1

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+ 
+    	for($i = 1; $i <= 5; $i++){
+ 
+    		DB::table('users')->insert([
+    			'name' => $faker->name,
+    			'email' => $faker->email,
+    			'password' => $faker->password()
+    		]);
+ 
+    	}
+    }
+}
